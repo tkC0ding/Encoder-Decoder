5eb59ae1d392739bd54cdc1e5df0b432709e42d4
tkC0ding <tkjagani@gmail.com>
2025-04-30 12:24:46 +0530

    Fineshed building helper functions

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import torch
 from model import Encoder, Decoder
 from utils import data_loader
 from train_eval_utils import train
+import pickle
 
+PATH = "data/preprocessed.pkl"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 hidden_size = 128
 batch_size = 32
+
+with open(PATH, 'rb') as file:
+    input_lang, output_lang, pairs = pickle.load(file)
 
 input_lang, output_lang, train_dataloader = data_loader(batch_size)
 
